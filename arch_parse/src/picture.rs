c669f29a40ed7e1a322c0f1449cf49efb3ffa52e
OmniTechnoMancer <OmniTechnoMancer@wah.quest>
2026-03-04 05:38:10 +1300

    arch_parse: Update pictures.rs from new info
    Further looking at the solaris 2.4.4 mpa software has revealed naming
    for the as yet unknown picture elements.  Also rename some things
    to better match how they are referred to in symbols.

--- arch_parse/src/picture.rs
+++ arch_parse/src/picture.rs
@@ -16,11 +16,11 @@ use crate::util::parse_length_string;
 #[derive(Debug, Default)]
 pub struct Picture {
     bounds: (i16, i16, i16, i16),
-    commands: Vec<Command>,
+    elements: Vec<Element>,
 }
 
 #[derive(Debug)]
-pub enum Command {
+pub enum Element {
     LineTo(i16, i16),
     MoveTo(i16, i16),
     Arc(u16, u16, u16, u16),
@@ -29,9 +29,10 @@ pub enum Command {
     TextRot(u32),
     Colour(u16),
     Text(String),
-    Unknown8(u32),
-    Unknown9(u32),
-    Unknown10(u16, u16),
+    FillType(u32),
+    LineType(u32),
+    PolyPoint(u16, u16),
+    PolyClose,
 }
 
 const LINETO: &[u8] = &[0_u8];
@@ -42,86 +43,94 @@ const LINEWIDTH: &[u8] = &[4_u8];
 const TEXTROT: &[u8] = &[5_u8];
 const COLOUR: &[u8] = &[6_u8];
 const TEXT: &[u8] = &[7_u8];
-const UNKNOWN8: &[u8] = &[8_u8];
-const UNKNOWN9: &[u8] = &[9_u8];
-const UNKNOWN10: &[u8] = &[10_u8];
+const FILLTYPE: &[u8] = &[8_u8];
+const LINETYPE: &[u8] = &[9_u8];
+const POLYPOINT: &[u8] = &[10_u8];
+const POLYCLOSE: &[u8] = &[11_u8];
 
-fn parse_lineto(input: &[u8]) -> IResult<&[u8], Command> {
+fn parse_lineto(input: &[u8]) -> IResult<&[u8], Element> {
     map(preceded(tag(LINETO), (be_i16, be_i16)), |(x, y)| {
-        Command::LineTo(x, y)
+        Element::LineTo(x, y)
     })
     .parse(input)
 }
 
-fn parse_moveto(input: &[u8]) -> IResult<&[u8], Command> {
+fn parse_moveto(input: &[u8]) -> IResult<&[u8], Element> {
     map(preceded(tag(MOVETO), (be_i16, be_i16)), |(x, y)| {
-        Command::MoveTo(x, y)
+        Element::MoveTo(x, y)
     })
     .parse(input)
 }
 
-fn parse_arc(input: &[u8]) -> IResult<&[u8], Command> {
+fn parse_arc(input: &[u8]) -> IResult<&[u8], Element> {
     map(
         preceded(tag(ARC), (be_u16, be_u16, be_u16, be_u16)),
-        |(x1, y1, x2, y2)| Command::Arc(x1, y1, x2, y2),
+        |(x1, y1, x2, y2)| Element::Arc(x1, y1, x2, y2),
     )
     .parse(input)
 }
 
-fn parse_textsize(input: &[u8]) -> IResult<&[u8], Command> {
+fn parse_textsize(input: &[u8]) -> IResult<&[u8], Element> {
     map(preceded(tag(TEXTSIZE), be_u32), |size| {
-        Command::TextSize(size)
+        Element::TextSize(size)
     })
     .parse(input)
 }
 
-fn parse_linewidth(input: &[u8]) -> IResult<&[u8], Command> {
+fn parse_linewidth(input: &[u8]) -> IResult<&[u8], Element> {
     map(preceded(tag(LINEWIDTH), be_u32), |width| {
-        Command::LineWidth(width)
+        Element::LineWidth(width)
     })
     .parse(input)
 }
 
-fn parse_textrot(input: &[u8]) -> IResult<&[u8], Command> {
-    map(preceded(tag(TEXTROT), be_u32), |rot| Command::TextSize(rot)).parse(input)
+fn parse_textrot(input: &[u8]) -> IResult<&[u8], Element> {
+    map(preceded(tag(TEXTROT), be_u32), |rot| Element::TextSize(rot)).parse(input)
 }
 
-fn parse_colour(input: &[u8]) -> IResult<&[u8], Command> {
+fn parse_colour(input: &[u8]) -> IResult<&[u8], Element> {
     map(preceded(tag(COLOUR), be_u16), |colour| {
-        Command::Colour(colour)
+        Element::Colour(colour)
     })
     .parse(input)
 }
 
-fn parse_text(input: &[u8]) -> IResult<&[u8], Command> {
+fn parse_text(input: &[u8]) -> IResult<&[u8], Element> {
     map(preceded(tag(TEXT), parse_length_string), |text| {
-        Command::Text(text)
+        Element::Text(text)
     })
     .parse(input)
 }
 
-fn parse_unknown8(input: &[u8]) -> IResult<&[u8], Command> {
-    map(preceded(tag(UNKNOWN8), be_u32), |unk| {
-        Command::Unknown8(unk)
+fn parse_fill_type(input: &[u8]) -> IResult<&[u8], Element> {
+    map(preceded(tag(FILLTYPE), be_u32), |unk| {
+        Element::FillType(unk)
     })
     .parse(input)
 }
 
-fn parse_unknown9(input: &[u8]) -> IResult<&[u8], Command> {
-    map(preceded(tag(UNKNOWN9), be_u32), |unk| {
-        Command::Unknown9(unk)
+fn parse_line_type(input: &[u8]) -> IResult<&[u8], Element> {
+    map(preceded(tag(LINETYPE), be_u32), |unk| {
+        Element::LineType(unk)
     })
     .parse(input)
 }
 
-fn parse_unknown10(input: &[u8]) -> IResult<&[u8], Command> {
-    map(preceded(tag(UNKNOWN10), (be_u16, be_u16)), |(x, y)| {
-        Command::Unknown10(x, y)
+fn parse_poly_point(input: &[u8]) -> IResult<&[u8], Element> {
+    map(preceded(tag(POLYPOINT), (be_u16, be_u16)), |(x, y)| {
+        Element::PolyPoint(x, y)
     })
     .parse(input)
 }
 
-fn parse_command(input: &[u8]) -> IResult<&[u8], Command> {
+fn parse_poly_close(input: &[u8]) -> IResult<&[u8], Element> {
+    map(tag(LINETYPE), |_| {
+        Element::PolyClose
+    })
+    .parse(input)
+}
+
+fn parse_element(input: &[u8]) -> IResult<&[u8], Element> {
     alt((
         parse_lineto,
         parse_moveto,
@@ -131,9 +140,10 @@ fn parse_command(input: &[u8]) -> IResult<&[u8], Command> {
         parse_textrot,
         parse_colour,
         parse_text,
-        parse_unknown8,
-        parse_unknown9,
-        parse_unknown10,
+        parse_fill_type,
+        parse_line_type,
+        parse_poly_point,
+        parse_poly_close,
     ))
     .parse(input)
 }
@@ -143,11 +153,11 @@ pub fn parse_picture_bin(input: &[u8]) -> IResult<&[u8], Option<Picture>> {
     if present != 0 {
         let (input, (x1, y1, x2, y2)) = (be_i16, be_i16, be_i16, be_i16).parse(input)?;
         //println!("Picture {} {} {} {}", x1, y1, x2, y2);
-        let (input, commands) = length_count(
+        let (input, elements) = length_count(
             be_u32,
-            map(dbg_dmp(parse_command, "command"), |cmd| {
-                //println!("{:?}", cmd);
-                cmd
+            map(dbg_dmp(parse_element, "element"), |elem| {
+                //println!("{:?}", elem);
+                elem
             }),
         )
         .parse(input)?;
@@ -155,7 +165,7 @@ pub fn parse_picture_bin(input: &[u8]) -> IResult<&[u8], Option<Picture>> {
             input,
             Some(Picture {
                 bounds: (x1, y1, x2, y2),
-                commands,
+                elements,
             }),
         ))
     } else {
